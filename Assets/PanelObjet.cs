f6dfedc1899076c9a06cc9ab24b7d53a5f86bff2
Haikal Danung Trinanda <77687182+hkldtrinanda@users.noreply.github.com>
2022-11-08 02:05:15 +0700

    Gameplay Scene - Quiz Gameplay & Core Gameplay

---
+++ Assets/PanelObjet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelObjet : MonoBehaviour
+{
+    public GameObject PanelOn, objectOff;
+    public GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        PanelOn.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void OnClickOn()
+    {
+        PanelOn.SetActive(true);
+        gameManager.counter++;
+        objectOff.SetActive(false);
+    }
+    
+}
