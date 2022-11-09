b01cb8faef5163149b93694d18f620b4d85a25cc
Haikal Danung Trinanda <77687182+hkldtrinanda@users.noreply.github.com>
2022-11-09 12:45:35 +0700

    Dialogue - UI - Audio

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeScene : MonoBehaviour
+{
+    public string sceneName, sceneName2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+    
+    public void OnClickOn()
+    {
+        SceneManager.LoadScene(sceneName2);
+    }
+    
+}
---
+++ Assets/Dm2.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class Dm2 : MonoBehaviour
+{
+    public Image actorImage;
+    public TextMeshProUGUI actorName;
+    public TextMeshProUGUI messageText;
+    public RectTransform backgroundBox;
+
+    Messagee[] currentMessages;
+    Actorr[] currentActors;
+    int activeMessage = 0;
+    public string scene;
+    [SerializeField]
+    public static bool isActive = false;
+
+    public void OpenDialogue(Messagee[] messages, Actorr[] actors){
+        currentMessages = messages;
+        currentActors = actors;
+        activeMessage = 0;
+        isActive = true;
+        Debug.Log("Started conversation! Loaded messages: " + currentMessages.Length);
+        DisplayMessage();
+        backgroundBox.LeanScale(Vector3.one, 0.5f);
+    }
+
+    void DisplayMessage()
+    {
+        Messagee messageToDisplay = currentMessages[activeMessage];
+        messageText.text = messageToDisplay.message;
+        Actorr actorToDisplay = currentActors[messageToDisplay.actorId];
+        actorName.text = actorToDisplay.name;
+        actorImage.sprite = actorToDisplay.sprite;
+        AnimateTextColor();
+    }
+
+    public void NextMessage()
+    {
+        activeMessage++;
+        if (activeMessage < currentMessages.Length)
+        {
+            DisplayMessage();
+        }
+        else
+        {
+            Debug.Log("Conversation Ended!");
+            backgroundBox.LeanScale(Vector3.zero, 0.5f).setEaseInOutExpo();
+            isActive = false;
+        }
+    }
+
+    void AnimateTextColor()
+    {
+        LeanTween.textAlpha(messageText.rectTransform, 0, 0);
+        LeanTween.textAlpha(messageText.rectTransform, 1, 0.5f);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        backgroundBox.transform.localScale = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isActive == true)
+        {
+            NextMessage();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isActive == false)
+        {
+            SceneManager.LoadScene(scene);
+            Debug.Log("Pindah Scene");
+        }
+       
+    }*/
+
+    public void PesanSelanjutnya()
+    {
+        NextMessage();
+    }
+}
