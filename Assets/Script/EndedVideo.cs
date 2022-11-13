77167f897344c0507e6f254a21f3b32f3616e014
Haikal Danung Trinanda <77687182+hkldtrinanda@users.noreply.github.com>
2022-11-14 00:15:23 +0700

    Added Instruction - Revision UI

--- Assets/Script/EndedVideo.cs
+++ Assets/Script/EndedVideo.cs
@@ -8,9 +8,14 @@ public class EndedVideo : MonoBehaviour
 {
     public VideoPlayer VideoPlayer;
     public string scene;// Drag & Drop the GameObject holding the VideoPlayer component
+
+    public float FrameRate = 60f;
+    public double Fframerate = 60f;
     void Start()
     {
         VideoPlayer.loopPointReached += LoadScene;
+        FrameRate = VideoPlayer.frameRate;
+        Fframerate = VideoPlayer.frameRate;
     }
     void LoadScene(VideoPlayer vp)
     {
